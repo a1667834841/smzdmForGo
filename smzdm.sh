956d73e874992f1a363014dfbace3704a4454269
ggball <1667834841@qq.com>
2022-04-24 16:56:02 +0800

    sh update

--- smzdm.sh
+++ smzdm.sh
@@ -11,7 +11,14 @@ elif  [ $1 = "stop" ]; then
 # 方法二：直接关闭进程kill  -9 [进程号]
 smzdm_pid=`ps -ef |grep smzdmPusher | grep -v 'grep\|stop' | awk '{print $2}' `
 kill -15 $smzdm_pid
-echo "$d mysql_m was stoped "
+echo "$d pusher was stoped "
+
+elif [ $1 = "reload" ]; then
+smzdm_pid=`ps -ef |grep smzdmPusher | grep -v 'grep\|stop' | awk '{print $2}' `
+kill -15 $smzdm_pid
+nohup ./smzdmPusher >> ./smzdm.log 2>&1 &
+echo "$d pusher was reloaded "
+
 else
 echo "输入错误，请检查重新输入"
 fi
