8c6085c611c1f4a8b07c733c0f43efe717b1654c
ggball <1667834841@qq.com>
2022-04-24 17:15:15 +0800

    sh update

--- smzdm.sh
+++ smzdm.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 d=`date "+%y-%m/%d %H:%M:%S"`
 #echo "$1$d"
-source /etc/profile
+
 if [ $1 = "start" ]; then
 echo "$d pusher is starting.... "
-        nohup ./smzdmPusher >> ./smzdm.log 2>&1 &
+source /etc/profile
+nohup ./smzdmPusher >> ./smzdm.log 2>&1 &
 
 elif  [ $1 = "stop" ]; then
 
